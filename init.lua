c68501dae8daf2437ee783171ece50b631abe791
bendzae <bendzae@gmail.com>
2023-05-08 11:32:20 +0200

    fix kotlin formatting

--- init.lua
+++ init.lua
@@ -43,10 +43,17 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
-      -- filter = function(client) -- fully override the default formatting function
-      --   return true
-      -- end
+      timeout_ms = 5000, -- default format timeout
+
+      filter = function(client)
+        -- only enable null-ls for kotlin files
+        if vim.bo.filetype == "kotlin" then
+          return client.name == "null-ls"
+        end
+
+        -- enable all other clients
+        return true
+      end
     },
     -- enable servers that you already have installed without mason
     servers = {
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,43 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  { "mg979/vim-visual-multi" },
+  {
+    "rebelot/heirline.nvim",
+    opts = function(_, opts)
+      local status = require("astronvim.utils.status")
+      opts.statusline = {
+        -- statusline
+        hl = { fg = "fg", bg = "bg" },
+        status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
+        status.component.git_branch(),
+        status.component.file_info { filetype = {}, filename = false, file_modified = false },
+        status.component.git_diff(),
+        status.component.diagnostics(),
+        status.component.fill(),
+        status.component.cmd_info(),
+        status.component.fill(),
+        status.component.lsp(),
+        status.component.treesitter(),
+        status.component.nav(),
+        -- remove the 2nd mode indicator on the right
+      }
+
+      -- return the final configuration table
+      return opts
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    config = function()
+      require("catppuccin").setup {}
+    end,
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    init = function() -- init function runs before the plugin is loaded
+      vim.g.sonokai_style = "andromeda"
+    end,
+  },
 }
