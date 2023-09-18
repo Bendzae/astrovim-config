7301f210800cf362dd76a174bc63e915ef575339
Ben <ben@bens-mbp.speedport.ip>
2023-09-18 21:53:41 +0200

    update some plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,12 +4,15 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
+  { import = "astrocommunity.pack.lua", enabled = true },
+  { import = "astrocommunity.pack.bash", enabled = true },
+  { import = "astrocommunity.pack.json", enabled = true },
   { import = "astrocommunity.pack.rust", enabled = true },
   { import = "astrocommunity.pack.wgsl", enabled = true },
   { import = "astrocommunity.pack.html-css", enabled = true },
   { import = "astrocommunity.pack.tailwindcss", enabled = true },
   { import = "astrocommunity.pack.typescript-all-in-one", enabled = true },
+  { import = "astrocommunity.colorscheme.catppuccin", enabled = true },
   { import = "astrocommunity.colorscheme.nightfox-nvim", enabled = true },
   { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
   { import = "astrocommunity.colorscheme.tokyonight-nvim", enabled = true },
