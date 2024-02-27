d4beaa075cf19b5f666cc95067b01d741d30599a
Ben <info@bendz.dev>
2024-02-27 11:13:51 +0100

    add some treesitter defaults

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,18 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+      "vim",
+      "C",
+      "rust",
+      "javascript",
+      "typescript",
+      "java",
+      "kotlin",
+      "bash",
+      "json",
+      "yaml",
+    },
   },
 }
